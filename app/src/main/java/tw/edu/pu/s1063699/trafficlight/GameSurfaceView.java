a46d8d4f4ff9715970cd99ed86a1210897760ddb
上機考 <ruru159321@gmail.com>
2020-06-05 14:32:55 +0800

    2020上機考版本0(黃志源)

--- app/src/main/java/tw/edu/pu/s1063699/trafficlight/GameSurfaceView.java
+++ app/src/main/java/tw/edu/pu/s1063699/trafficlight/GameSurfaceView.java
@@ -10,8 +10,7 @@ import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-
+//555
 public class GameSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
     public GameSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
