9363d700ff404fc56e34c3b4da09130751e85de4
上機考 <ruru159321@gmail.com>
2020-06-05 14:19:41 +0800

    2020上機考版本0(黃志源)

--- app/src/main/java/tw/edu/pu/s1063699/trafficlight/GameActivity.java
+++ app/src/main/java/tw/edu/pu/s1063699/trafficlight/GameActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 public class GameActivity extends AppCompatActivity {
     GameSurfaceView GameSV;
     Handler handler;
-
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
